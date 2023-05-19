7e3e7b57d7fb06294915f6b0a71b087fc31cce41
vishalrajole <vishalrajole1991@gmail.com>
2023-05-19 08:36:28 +0200

    Added styled components

---
+++ README.md
@@ -0,0 +1,12 @@
+# React TypeScript Vite setup
+
+# Stack
+
+React + TypeScript + Vite + Styled-components
+
+# Setup
+
+- clone repository
+- run `yarn` to install dependencies
+- run `yarn dev` to run web app locally on `http://localhost:3000`
+- run `yarn build` to build production app
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    server: {
+      port: 3000,
+    },
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            [
+              "babel-plugin-styled-components",
+              {
+                displayName: true,
+                ssr: false,
+                pure: true,
+                fileName: false,
+              },
+            ],
+          ],
+        },
+      }),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+        overlay: {
+          initialIsOpen: false,
+        },
+      }),
+    ],
+  };
 });
